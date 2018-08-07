cee4d1e2cea7a2c720d4fd4cf3095d610decfa18
Ville Hiltunen <hiltunenvillej@gmail.com>
2018-08-08 02:18:07 +0300

    Put includes in, stumbled with bugs

--- application/leds.c
+++ application/leds.c
@@ -1,1 +1,6 @@
-//empty skeleton right now
+/* Copyright Ville Hiltunen 2018 <hiltunenvillej@gmail.com>
+ *
+ * All code here is public domain.
+ */
+
+#include "leds.h"
--- application/leds.h
+++ application/leds.h
@@ -1,1 +1,23 @@
-//another empty skeleton
+/* Copyright Ville Hiltunen 2018 <hiltunenvillej@gmail.com>
+ *
+ * All code is public domain here.
+ */
+#ifndef LEDDRIVERH
+#define LEDDRIVERH
+
+#include "apa102.h"
+#include "apa102_params.h"
+#include "xtimer.h"
+#include "color.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//stuff here
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //Include guard
